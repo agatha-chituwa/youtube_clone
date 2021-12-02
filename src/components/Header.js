0b172e4c2cef0c5dbd91d6865772d1650e6c0b2d
agatha-chituwa <bsc-com-06-18@cc.ac.mw>
2021-12-02 22:27:21 +0400

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import './Header.css'
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search'
+import VideoCallIcon from '@mui/icons-material/VideoCall'
+import AppsIcon from '@mui/icons-material/Apps'
+import NotificationsIcon from'@mui/icons-material/Notifications'
+
+
+function Header() {
+    return (
+        <div className="header">
+
+            <div className="left">
+            <MenuIcon/>
+            <img  className="header_logo" src="/youtube.png" alt="" />
+            </div>
+           
+           <div className="header_input">
+           <input type="text"  placeholder="search"/> 
+            <SearchIcon className="header_input_icon"/>
+           </div>
+
+           <div className="header_icon">
+           <VideoCallIcon className="header_icons"/>
+            <AppsIcon className="header_icons"/>
+            <NotificationsIcon className="header_icons"/>
+           </div>
+           
+           
+           
+            
+        </div>
+    )
+}
+
+export default Header
